dd1139a29184066338bfdeab7486ccc851c86177
ankurvc <ankur.patel@viitor.cloud>
2021-10-22 21:30:42 +0530

    node sample

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express(); app.listen(3000, function () {
+    console.log("listening on 3000");
+}); app.get("/", (req, res) => {
+    res.send("Ankur Patel -- API V4");
+}); app.get("/delete", (req, res) => {
+    res.send("Delete User");
+}); app.get("/update", (req, res) => {
+    res.send("Update User");
+}); app.get("/insert", (req, res) => {
+    res.send("Insert User");
+});
